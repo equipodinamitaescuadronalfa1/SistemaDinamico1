2cca5cc59f606e5158d43588ee26b2d41bd6a521
nek400 <dcc.david.calderon@gmail.com>
2019-09-05 15:00:32 -0500

    Solucion de errores

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ import sys
 
 #./dynamic -f formulae.dat
 # USAGE
-#./dynamic.py -e a*x^n+b -a 1 10 1  -b 1 5 1 -n 2 3 1 -x0 0.1 1.0 0.1 -i 6
+#python3 main.py -e a*x^n+b -a 1 10 1  -b 1 5 1 -n 2 3 1 -x0 0.1 1.0 0.1 -i 6
 #from readcol import fgetcols
 
 def plotting(fig,ax,time,y1,title,filename,caption):
